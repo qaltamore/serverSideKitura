3d40dd5436d7c95cc92ede9dc761bf8b50dab8d7
Altamore Quentin <quentin.altamore@estiam.com>
2019-03-19 16:00:59 +0100

    Interface des commandes OK

--- Sources/PizzeKitchen/main.swift
+++ Sources/PizzeKitchen/main.swift
@@ -11,11 +11,24 @@ struct Pizza: Codable {
     var ingredients: [String]
 }
 
+struct Order: Codable {
+    var numero: Int
+    var name: String
+}
+
 var listCalzoneIngredients = ["oeuf", "jambon", "mozarella", "sauce tomate"]
 var listQuatreFromagesIngredients = ["gorgonzola", "mozarella", "chèvre", "parmesan", "sauce tomate"]
 var pizzeList = [
     Pizza(name: "Calzone", ingredients: listCalzoneIngredients),
     Pizza(name: "4 Frômages", ingredients: listQuatreFromagesIngredients)
+]
+
+var orders = [Order]()
+orders = [
+    Order(numero: 0, name: "Calzone"),
+    Order(numero: 1, name: "4 Frômages"),
+    Order(numero: 2, name: "Regina"),
+    Order(numero: 3, name: "Calzone")
 ]
 
 
@@ -44,17 +57,17 @@ router.post("/newRecipe") { request, response, next in
     print(request.body ?? "Body is nil")
     
     if let body = request.body?.asURLEncoded {
-        if let nameRecipe: String = body["nameRecipe"] as! String, let ingredientsRecipe: String = body["ingredientsRecipe"] as! String {
-            print("Pizza : ", nameRecipe)
-            print("Ingrédients : ", ingredientsRecipe)
-            var ingredientsRecipeTab = ingredientsRecipe.components(separatedBy: ",")
+        if let nameRecipe = body["nameRecipe"], let ingredientsRecipe = body["ingredientsRecipe"] {
+            let ingredientsRecipeTab = ingredientsRecipe.components(separatedBy: ",")
             var ingredientsRecipeTabTrimmed: [String] = []
+            
             for item in ingredientsRecipeTab {
                 ingredientsRecipeTabTrimmed.append(item.trimmingCharacters(in: .whitespacesAndNewlines))
             }
             
-            var pizza = Pizza(name: nameRecipe, ingredients: ingredientsRecipeTabTrimmed)
+            let pizza = Pizza(name: nameRecipe, ingredients: ingredientsRecipeTabTrimmed)
             pizzeList.append(pizza)
+            
             try response.render("Home.stencil", context: ["pizzeList": pizzeList])
         }
     } else {
@@ -65,10 +78,42 @@ router.post("/newRecipe") { request, response, next in
 }
 
 router.get("/orders") { request, response, next in
-    let orders = [String]()
     try response.render("Orders.stencil", context: ["orders": orders])
+    next()
+}
+
+router.post("/orders") { request, response, next in
+    
+    print(request.body ?? "Body is nil")
+    
+    if let body = request.body?.asURLEncoded {
+        if let deliveredPizze = body["pizze"] {
+            print("deliveredPizze: ", deliveredPizze)
+            let deliveredPizzeTab = deliveredPizze.components(separatedBy: ",")
+            
+            for pizza in deliveredPizzeTab {
+                var pizzaIndex: Int! = Int(pizza)
+                orders.remove(at: pizzaIndex)
+            }
+            
+            var tmpTab = orders
+            for (index, order) in orders.enumerated() {
+                tmpTab[index].numero = index
+            }
+            orders = tmpTab
+        }
+    }
+    
+    try response.render("Orders.stencil", context: ["orders": orders])
+    
     next()
 }
 
 Kitura.addHTTPServer(onPort: 8080,  with: router)
 Kitura.run()
+
+/*extension Array {
+    mutating func remove(_ element: Element) {
+        self = self.filter { $0 != element }
+    }
+}*/
