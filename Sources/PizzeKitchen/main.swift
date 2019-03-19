7525a17e2ec064f1bef80669a75b338dfd9ac7f1
Altamore Quentin <quentin.altamore@estiam.com>
2019-03-19 11:25:50 +0100

    Affichage des recettes sur la HomePage + les boutons qui redirigent vers les pages appropriées

--- Sources/PizzeKitchen/main.swift
+++ Sources/PizzeKitchen/main.swift
@@ -12,24 +12,66 @@ struct HelloResponse : Codable {
 
 let router = Router()
 
+/*var pizzeList = [{
+    name: "Calzone",
+    ingredients: [
+        "oeuf",
+        "jambon",
+        "mozarella",
+        "sauce tomate"
+    ]
+    }]*/
+struct Pizza: Codable {
+    var name: String
+    var ingredients: [String]
+}
+
+var listCalzoneIngredients = ["oeuf", "jambon", "mozarella", "sauce tomate"]
+var listQuatreFromagesIngredients = ["gorgonzola", "mozarella", "chèvre", "parmesan", "sauce tomate"]
+let pizzeList = [
+    Pizza(name: "Calzone", ingredients: listCalzoneIngredients),
+    Pizza(name: "4 Frômages", ingredients: listQuatreFromagesIngredients)
+]
+
+
 // middleware = truc qui va travailler à ma place
 router.all(middleware: [BodyParser(), StaticFileServer(path: "./Public")]) //Tu regardes le body, si c'est du JSON tu me renvoies du JSON, si c'est de l'XML tu me renvoies de l'XML, etc
 router.add(templateEngine: StencilTemplateEngine())
 
-router.get("/:ingredient") { request, response, next in
+/* on  garde en exemple pour le moment
+ 
+ if let ingredient = request.parameters["ingredient"]
+ 
+ 
+ router.post("/api/hello") { request, response, next in
+    print(request.body ?? "Body is nil")
+    //il n'y a potentiellement pas de body dans une request
+    if let body = request.body?.asJSON {
+        print("Oui")
+        if let p = body["prenom"], let n = body["nom"] {
+            print("Test")
+            response.send(HelloResponse(result: "Hello \(p) \(n)"))
+        }
+    } else {
+        response.status(.notFound)
+    }
+    
+    next()
+}*/
+
+router.get("/") { request, response, next in
+    try response.render("Home.stencil", context: ["pizzeList": pizzeList])
+    next()
+}
+
+router.get("/newRecipe") { request, response, next in
     let n : String
     if let ingredient = request.parameters["ingredient"] {
         n = ingredient
     } else {
         n = ""
     }
-    response.send(n)
-    //try response.render("Hello.stencil", context: ["ingredient": n])
-    next() //j’ai fini, je passe la main
-}
-
-router.get("/") { request, response, next in
-    response.send("Bonjour")
+    try response.render("NewRecipe.stencil")
     next()
 }
 
